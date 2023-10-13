39d7669545e0546802431fa6be63329afd00d5a8
Ferr <ursusmartimus007@gmail.com>
2023-10-13 16:35:08 +0300

    added components in fragments

--- app/src/main/java/com/example/coursework4chapter/ui/view/fragments/AddStudentFragment.java
+++ app/src/main/java/com/example/coursework4chapter/ui/view/fragments/AddStudentFragment.java
@@ -12,10 +12,10 @@ import com.example.coursework4chapter.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link RegisterFragment#newInstance} factory method to
+ * Use the {@link AddStudentFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RegisterFragment extends Fragment {
+public class AddStudentFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class RegisterFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public RegisterFragment() {
+    public AddStudentFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class RegisterFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
+     * @return A new instance of fragment SettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
+    public static AddStudentFragment newInstance(String param1, String param2) {
+        AddStudentFragment fragment = new AddStudentFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        return inflater.inflate(R.layout.fragment_add_student, container, false);
     }
 }
