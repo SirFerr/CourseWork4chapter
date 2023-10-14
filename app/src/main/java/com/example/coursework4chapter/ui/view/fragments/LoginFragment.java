07c15bbfb6e6508336ec3ee9605d9c359a209de1
Ferr <ursusmartimus007@gmail.com>
2023-10-14 13:14:51 +0300

    added list and small realization of LoginFragment

--- app/src/main/java/com/example/coursework4chapter/ui/view/fragments/LoginFragment.java
+++ app/src/main/java/com/example/coursework4chapter/ui/view/fragments/LoginFragment.java
@@ -3,64 +3,40 @@ package com.example.coursework4chapter.ui.view.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.coursework4chapter.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    View view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onResume() {
+        super.onResume();
+        EditText email= view.findViewById(R.id.editTextEmailLogin);
+        EditText password= view.findViewById(R.id.editTextPasswordLogin);
+        Button login = view.findViewById(R.id.btnLoginLogin);
+        Button signup = view.findViewById(R.id.btnSignUpLogin);
+
+        signup.setOnClickListener(v->{
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registerFragment);
+        });
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        view=inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
     }
 }
